c501be3db6072db68e5ffe4afffd638c40895373
Dias1029 <103851381+Dias1029@users.noreply.github.com>
2023-11-27 16:11:51 +0300

    Second Commit

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthController : MonoBehaviour
+public class Health : MonoBehaviour
 {
     [SerializeField] private Slider _healthBar;
     [SerializeField] private float _health;
